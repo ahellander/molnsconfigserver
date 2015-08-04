f86ba3b2b0be920e97e7ac153e5f42f97ee92304
Ben Bales <bbbales2@gmail.com>
2015-08-03 17:38:59 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,140 @@
+$( function() {
+    var AppView = Backbone.View.extend({
+        el : $(".content")[0],
+
+        // Delegated events for creating new items, and clearing completed ones.
+        events : {
+            "click .startCluster" :  "startCluster",
+            "click .stopCluster" : "stopCluster"
+        },
+
+        // At initialization we bind to the relevant events on the `Todos`
+        // collection, when items are added or changed. Kick things off by
+        // loading any preexisting todos that might be saved in *localStorage*.
+        initialize : function() {
+        },
+
+        pollStdout : _.once(function() {
+            var inner = _.bind(function() {
+                $.ajax( { url : '/readstdout',
+                          data : {},
+                          success : _.bind(_.partial(function(inner, data) {
+                              for(var i = 0; i < data.length; i++)
+                              {
+                                  var msg = '';
+                                  
+                                  for(var c in data[i].msg)
+                                  {
+                                      if(data[i].msg[c] == '\n')
+                                      {
+                                          if(msg.length > 0)
+                                              this.handleMessage({ status : data[i].status, msg : msg });
+
+                                          msg = ''
+
+                                          this.createMessage();
+                                      }
+                                      else
+                                      {
+                                          msg += data[i].msg[c];
+                                      }
+                                  }
+                                  
+                                  if(msg.length > 0)
+                                      this.handleMessage({ status : data[i].status, msg : msg });
+                              }
+                           
+                              setTimeout(_.bind(inner, this), 1000);
+                          }, arguments.callee), this),
+                          error : _.bind(_.partial(function(inner, data) {
+                              setTimeout(_.bind(inner, this), 1000);
+                          }, arguments.callee), this),
+                          method : 'POST',
+                          dataType : 'json'
+                        });
+            }, this);
+            
+            inner();
+        }),
+                            
+        startCluster : function() {
+            $.post( '/startmolns',
+                    {
+                        aws_access_key : $( 'input[name=aws_access_key]' ).val(),
+                        aws_secret_key : $( 'input[name=aws_secret_key]' ).val(),
+                        head_node : $( '.headNode' ).val()
+                    },
+                    _.bind(function(data) {
+                        this.createMessage(data);
+                    }, this),
+                    "json"
+                  );
+        },
+
+        stopCluster : function() {
+            $.post( '/stopmolns',
+                    {},
+                    _.bind(function(data) {
+                        this.createMessage(data);
+                    }, this),
+                    "json"
+                  );
+        },
+
+        createMessage : function(data) {
+            var dateString = '';
+
+            var date = new Date();
+            var dateString = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+            if(typeof data != "undefined")
+                this.handleMessage(data);
+
+            $( '.messages' ).append( '<div style="display: none;" class="line"><span class="time">' + dateString + '</span>: <pre style="display : inline;" class="content"></pre></div>' );
+        },
+
+        handleMessage : function(data) {
+            var line = $( '.messages>div.line' ).last();
+
+            line.show();
+
+            var element = line.find( 'pre.content' );
+            var time = line.find( 'span.time' );
+
+            var date = new Date();
+
+            time.text(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
+
+            if(data.status)
+            {
+                element.append( '<span><font color="green">' + data.msg + '</font></span>' );
+            } else {
+                element.append( '<span><font color="red">' + data.msg + '</font></span>' );
+            }
+        },
+
+        // Re-rendering the App just means refreshing the statistics -- the rest
+        // of the app doesn't change.
+        render : function() {
+            var data = JSON.parse($( '.jsonData' ).text());
+
+            this.$el.find( 'input[name=aws_access_key]' ).val(data['aws_access_key']);
+            this.$el.find( 'input[name=aws_secret_key]' ).val(data['aws_secret_key']);
+
+            this.$el.find( '.headNode>option[value="' + data['head_node'] + '"]' ).prop('selected', true);
+
+            $( '.loading' ).hide();
+
+            this.delegateEvents();
+
+            this.createMessage();
+            this.pollStdout();
+
+            this.$el.show();
+        }
+    });
+
+    var App = new AppView();
+
+    App.render();
+} );
---
+++ main.py
@@ -0,0 +1,275 @@
+import cherrypy
+import jinja2
+import json
+import molns
+import multiprocessing
+import os
+import Queue
+import traceback
+
+appDir = os.path.dirname(os.path.abspath(__file__))
+
+templateLoader = jinja2.FileSystemLoader(searchpath = "html")
+templateEnv = jinja2.Environment(loader = templateLoader)
+
+class Logger(object):
+    def __init__(self, queue):
+        self.queue = queue
+
+    def write(self, stuff):
+        self.queue.put(stuff)
+
+    def flush(self):
+        pass
+
+def wrapStdoutStderr(func, stdout, stderr, args = (), kwargs = {}):
+    import sys
+    import molns
+    sys.stdout = stdout
+    sys.stderr = stderr
+
+    print 'wtf', kwargs
+
+    if 'config' in kwargs:
+        kwargs['config'] = molns.MOLNSConfig(db_file = kwargs['config'])
+
+    print args, kwargs
+
+    try:
+        func(*args, **kwargs)
+    except Exception as e:
+        traceback.print_exc()
+        print str(e)
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+def logexceptions(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            traceback.print_exc()
+
+            return { 'status' : False, 'msg' : str(e) }
+
+    return inner
+
+def testfunction(a, s, d, f):
+    print a
+    print s
+    print d
+    print f
+
+#if __name__ == '__main__':
+if False:
+    stdout = Logger(multiprocessing.Queue())
+    stderr = Logger(multiprocessing.Queue())
+    process = multiprocessing.Process(target = wrapStdoutStderr, args = (testfunction, stdout, stderr, ('a', 'b', 'c', 'd')))
+
+    process.start()
+
+    i = 0
+
+    #print stdout.queue.get(False)
+
+    while True:
+        try:
+            res = stdout.queue.get(False)
+            print 'o', i, res
+            i += 1
+        except Queue.Empty as e:
+            break
+
+    while True:
+        try:
+            res = stderr.queue.get(False)
+            print 'e', i, res
+            i += 1
+        except Queue.Empty as e:
+            break
+
+    process.join()
+
+    while True:
+        try:
+            res = stdout.queue.get(False)
+            print 'o', i, res
+            i += 1
+        except Queue.Empty as e:
+            break
+
+    while True:
+        try:
+            res = stderr.queue.get(False)
+            print 'e', i, res
+            i += 1
+        except Queue.Empty as e:
+            break
+
+class App(object):
+    @cherrypy.expose
+    @logexceptions
+    def index(self):
+        template = templateEnv.get_template( 'index.html' )
+
+        data = {}
+
+        if 'aws_access_key' not in cherrypy.session:
+            data['aws_access_key'] = ''
+        else:
+            data['aws_access_key'] = cherrypy.session['aws_access_key']
+
+        if 'aws_secret_key' not in cherrypy.session:
+            data['aws_access_key'] = ''
+        else:
+            data['aws_secret_key'] = cherrypy.session['aws_secret_key']
+
+        if 'head_node' not in cherrypy.session:
+            data['head_node'] = 'c3.large'
+        else:
+            data['head_node'] = cherrypy.session['head_node']
+
+        return template.render({ 'json' : json.dumps(data) })
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @logexceptions
+    def savekeys(self, aws_access_key = None, aws_secret_key = None):
+        if aws_access_key == None:
+            return { 'status' : False, 'msg' : 'Access key must be set' }
+
+        if aws_secret_key == None:
+            return { 'status' : False, 'msg' : 'Secret key must be set' }
+
+        if 'aws_access_key' not in cherrypy.session:
+            cherrypy.session['aws_access_key'] = aws_access_key
+
+        if 'aws_secret_key' not in cherrypy.session:
+            cherrypy.session['aws_secret_key'] = aws_secret_key
+
+        config = molns.MOLNSConfig(db_file="/home/bbales2/molnsconfigserver/test.db")
+        provider_conf_items = molns.MOLNSProvider.provider_get_config(provider_type='EC2', config = config)
+
+        json_obj = {}
+
+        for obj in provider_conf_items:
+            json_obj[obj['key']] = obj['value']
+
+        json_obj['aws_access_key'] = aws_access_key
+        json_obj['aws_secret_key'] = aws_secret_key
+
+        molns.MOLNSProvider.provider_import('', config, json_obj)
+
+        return { 'status' : True, 'msg' : 'Keys set successfully' }
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @logexceptions
+    def readstdout(self):
+        if 'process' not in cherrypy.session:
+            return []
+
+        process, stdout, stderr = cherrypy.session['process']
+
+        output = []
+
+        while True:
+            try:
+                res = stdout.queue.get(False)
+                output.append({ 'status' : 1, 'msg' : res })
+            except Queue.Empty as e:
+                break
+
+        while True:
+            try:
+                res = stderr.queue.get(False)
+                print res
+                output.append({ 'status' : 0, 'msg' : res })
+            except Queue.Empty as e:
+                break
+
+        return output
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @logexceptions
+    def startmolns(self, aws_access_key = None, aws_secret_key = None, head_node = None):
+        if 'process' in cherrypy.session and cherrypy.session['process'][0].is_alive():
+            return { 'status' : False, 'msg' : 'Currently running process' }
+        else:
+            session = None
+
+        if aws_access_key == None:
+            return { 'status' : False, 'msg' : 'Access key must be set' }
+
+        if aws_secret_key == None:
+            return { 'status' : False, 'msg' : 'Secret key must be set' }
+
+        if head_node == None:
+            return { 'status' : False, 'msg' : 'Head node must be set' }
+
+        if 'aws_access_key' not in cherrypy.session:
+            cherrypy.session['aws_access_key'] = aws_access_key
+
+        if 'aws_secret_key' not in cherrypy.session:
+            cherrypy.session['aws_secret_key'] = aws_secret_key
+
+        if 'head_node' not in cherrypy.session:
+            cherrypy.session['head_node'] = head_node
+
+        config = molns.MOLNSConfig(db_file="/home/bbales2/molnsconfigserver/test.db")
+        provider_conf_items = molns.MOLNSProvider.provider_get_config(provider_type = 'EC2', config = config)
+
+        json_obj = { 'name' : 'mountain',
+                     'type' : 'EC2',
+                     'config' : {} }
+
+        json_obj['config']['aws_access_key'] = aws_access_key
+        json_obj['config']['aws_secret_key'] = aws_secret_key
+
+        molns.MOLNSProvider.provider_import('', config, json_obj)
+
+        stdout = Logger(multiprocessing.Queue())
+        stderr = Logger(multiprocessing.Queue())
+        #process = multiprocessing.Process(target = wrapStdoutStderr, args = (testfunction, stdout, stderr, ('a', 'b', 'c', 'd')))
+        #process.join()
+        #molns.MOLNSProvider.provider_initialize('mountain', config)
+        process = multiprocessing.Process(target = wrapStdoutStderr, args = (molns.MOLNSProvider.provider_initialize, stdout, stderr, [json_obj['name']], { 'config' : "/home/bbales2/molnsconfigserver/test.db" }))
+        #process = multiprocessing.Process(target = molns.MOLNSProvider.provider_initialize, args = (json_obj['name'], config))
+        process.start()
+
+        cherrypy.session['process'] = (process, stdout, stderr)
+
+        molns.MOLNSProvider.provider_get_config(provider_type='EC2', config = config)
+
+        controller_conf_items = molns.MOLNSController.controller_get_config(provider_type = 'EC2', config = config)
+
+        json_obj = { 'name' : 'goat',
+                     'provider_name' : 'mountain',
+                     'config' : { 'instance_type' : head_node} }
+
+        molns.MOLNSController.controller_import('', config, json_obj)
+
+        
+
+        #molns
+        #conf3 = molns.MOLNSWorkerGroup.worker_group_get_config(provider_type='Eucalyptus', config=config)
+
+        return { 'status' : True, 'msg' : 'MOLNs started successfully' }
+
+if __name__ == '__main__':
+    cherrypy.quickstart(App(), '/', {
+        '/' : {
+            'tools.gzip.on' : True,
+            'log.screen' : True,
+            'tools.sessions.on' : True
+        },
+        '/js' : {
+            'tools.staticdir.on' : True,
+            'tools.staticdir.dir' : os.path.join(appDir, 'js'),
+            'log.screen' : True
+        },
+    })
+
+    
